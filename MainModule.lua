ab94b54e869c7fd0511a7e1cc48b06c3330e2ff9
Alsfff <als.geno92@gmail.com>
2024-12-24 11:49:28 +0100

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -6,7 +6,7 @@ local Slider = require(script.Components.Slider)
 local Toggle = require(script.Components.Toggle)
 
 -- Méthode pour créer une interface
-function MyLibrary:CreateUI(title)
+function VinuxUI:CreateUI(title)
     local screenGui = Instance.new("ScreenGui")
     screenGui.Name = title
     screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -14,18 +14,18 @@ function MyLibrary:CreateUI(title)
 end
 
 -- Ajouter un bouton
-function MyLibrary:CreateButton(parent, text, callback)
+function VinuxUI:CreateButton(parent, text, callback)
     return Button.new(parent, text, callback)
 end
 
 -- Ajouter un slider
-function MyLibrary:CreateSlider(parent, min, max, callback)
+function VinuxUI:CreateSlider(parent, min, max, callback)
     return Slider.new(parent, min, max, callback)
 end
 
 -- Ajouter un toggle
-function MyLibrary:CreateToggle(parent, text, callback)
+function VinuxUI:CreateToggle(parent, text, callback)
     return Toggle.new(parent, text, callback)
 end
 
-return MyLibrary
+return VinuxUI
